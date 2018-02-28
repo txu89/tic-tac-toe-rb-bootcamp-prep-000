00e940ac3b0acb71f67a63ff71635800de0e0dfb
Tom Xu <tomxu89@gmail.com>
2018-02-28 20:13:35 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -9,3 +9,11 @@ WIN_COMBINATIONS = [
   [2,4,6]
 ]
 
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
