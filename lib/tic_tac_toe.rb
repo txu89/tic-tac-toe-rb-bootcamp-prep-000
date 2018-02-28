371c53d6ae5f47163ce2b369bbe131874722ec2e
Tom Xu <tomxu89@gmail.com>
2018-02-28 20:15:04 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -50,3 +50,13 @@ def turn(board)
   end
 end
 
+	+def turn_count(board) 
++  counter = 0 
++  board.each { | position |
++    if position == 'X' || position == 'O'
++      counter += 1 
++    end
++  }
++  return counter
++end
+
