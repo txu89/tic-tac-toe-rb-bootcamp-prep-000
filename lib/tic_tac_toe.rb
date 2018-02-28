b1086b86ea935144f5dd64a2dc4aa83312a5b75b
Tom Xu <tomxu89@gmail.com>
2018-02-28 20:49:03 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -115,7 +115,7 @@ def play(board)
   end
   
   if (won?(board))
-    puts "Congratulations #{winner(board)}"
+    puts "Congratulations #{winner(board)}!"
   elsif (draw?(board))
     puts "Cat's Game!"
   end
